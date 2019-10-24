688d4bfa7c455d34bc20b892193578900098205b
hungavcodegym <hungavcodegym>
2019-10-24 10:36:18 +0700

    'add'

---
+++ src/CircleMain.java
@@ -0,0 +1,6 @@
+public class CircleMain {
+    public static void main(String[] args) {
+        Circle circle = new Circle(2, "red");
+        System.out.println(circle.toString());
+    }
+}
---
+++ src/CylinderMain.java
@@ -0,0 +1,6 @@
+public class CylinderMain {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder(3, "blue", 4);
+        System.out.println(cylinder.toString());
+    }
+}
